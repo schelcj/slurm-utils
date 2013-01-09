88fa413b895227f778033425d9ab1793e4f1614d
Chris Scheller <schelcj@umich.edu>
2013-01-09 11:17:45 -0500

    just quicky to help setting the grpcpus values

---
+++ utils/cluster_set_grpcpus.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Slurm;
+use Getopt::Compact;
+
+my $opts        = Getopt::Compact->new(struct => [[[qw(p percentage)], q{Percentage of total cores}, q{=i}]])->opts();
+
+warn "Percentage required" if not $opts->{percentage};
+exit 1;
+
+my $qos         = 'biostat';
+my $slurm       = Slurm::new();
+my $nodes       = $slurm->load_node();
+my $total_cores = 0;
+
+map {$total_cores += $_->{cpus}} @{$nodes->{node_array}};
+
+my $grpcpus = sprintf '%0.f', $total_cores * (0.01 * $opts->{percentage});
+
+say "sacctmgr update qos name=$qos set grpcpus=$grpcpus";

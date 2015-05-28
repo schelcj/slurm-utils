553f37be3bb40bd185a34d81b9236cdb828c417c
Chris Scheller <schelcj@pobox.com>
2015-05-28 13:12:50 -0400

    spacing issues with longer hostnames

--- utils/cluster_status.pl
+++ utils/cluster_status.pl
@@ -7,8 +7,8 @@ use Slurm qw(:constant);
 use feature qw(say);
 
 my $INT     = 2**31;
-my $HR      = q{-} x 96;
-my $ROW_FMT = qq{%-10s %-10s %-10s %-15s %-10s %-10s %-15s %-10s\n};
+my $HR      = q{-} x 106;
+my $ROW_FMT = qq{%-20s %-10s %-10s %-15s %-10s %-10s %-15s %-10s\n};
 my @HEADERS = (qw(Node AllocCPU TotalCPU PercentUsedCPU AllocMem TotalMem PercentUsedMem NodeState));
 my $slurm   = Slurm::new();
 my $nodes   = $slurm->load_node();
